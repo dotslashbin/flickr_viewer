e77eaec673e0103040b29b5010dd738e05560a32
Joshua Fuentes <joshuarpf@gmail.com>
2019-10-07 09:48:04 +0800

    revised to return the proper JSON format

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,3 +1,3 @@
 export default {
-	FLIKR_API_URL: 'https://www.flickr.com/services/feeds/photos_public.gne?format=json'
+	FLIKR_API_URL: 'https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1'
 }
