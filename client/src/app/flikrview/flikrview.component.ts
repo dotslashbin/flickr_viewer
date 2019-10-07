1704e358cbe921ebbda34c88a270e38411b657e7
Joshua Fuentes <joshuarpf@gmail.com>
2019-10-07 15:17:03 +0800

    Added display of images and search field

--- client/src/app/flikrview/flikrview.component.ts
+++ client/src/app/flikrview/flikrview.component.ts
@@ -2,19 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { FlickrService } from '../flickr.service';
 
 @Component({
-  selector: 'app-flikrview',
-  templateUrl: './flikrview.component.html',
-  styleUrls: ['./flikrview.component.css']
+	selector: 'app-flikrview',
+	templateUrl: './flikrview.component.html',
+	styleUrls: ['./flikrview.component.css']
 })
+
 export class FlikrviewComponent implements OnInit {
+
+	pictures: any[];
 
   	constructor(private flickrService: FlickrService) { }
 
   	ngOnInit() {
-		this.flickrService.getImages().subscribe((images) => {
-			console.log('you are here...')
-			console.log(images);
-		})
+		this.flickrService.getImages().subscribe((result) => {
+			if (result.type === 'success') {
+				this.pictures = result.data;
+			}
+		});
+	}
+
+	searchForPic(searchString) {
+		console.log(`you will search now ... ${searchString}`);
 	}
 
 }
